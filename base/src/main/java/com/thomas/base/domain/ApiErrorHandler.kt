e03bde0b465336aeb9d578da6c44286f06d15115
thomasmei <thomasmei@163.com>
2026-04-11 16:54:52 +0800

    update domain base

--- base/src/main/java/com/thomas/base/domain/ApiErrorHandler.kt
+++ base/src/main/java/com/thomas/base/domain/ApiErrorHandler.kt
@@ -1,5 +1,8 @@
 package com.thomas.base.domain
 
+import okio.IOException
+import retrofit2.HttpException
+
 /**
  * Created by thomas on 4/11/2026.
  */
@@ -17,12 +20,32 @@ object ApiErrorHandler : ErrorHandler {
     const val UNKNOWN_ERROR_STR = "Unknown error"
     const val ERROR_STR = "Error"
 
+    fun getErrorMessage(code: Int, message: String? = null): String {
+        val errorMsg = message ?: ""
 
-    override fun <T> toError(response: Response<T>): Result.Error {
-        return Result.Error(response.code, "API Error")
+        return when (code) {
+            SYSTEM_ERROR -> "$SYSTEM_ERROR_STR: $errorMsg"
+            NETWORK_ERROR -> "$NETWORK_ERROR_STR: $errorMsg"
+            NO_CONTENT_ERROR -> "$NO_CONTENT_ERROR_STR: $errorMsg"
+            else -> "$ERROR_STR $code: ${message ?: UNKNOWN_ERROR_STR}"
+        }
     }
 
-    override fun toError(e: Exception): Result.Error {
-        return Result.Error(-1, e.message ?: "Unknown error")
+    override fun <T> toError(response: Response<T>): Result.Error {
+        return if (response.isSuccess() && response.data == null) {
+            Result.Error(NO_CONTENT_ERROR, getErrorMessage(NO_CONTENT_ERROR, response.message))
+        } else {
+            Result.Error(response.code, response.message ?: BUSINESS_ERROR_STR)
+        }
+    }
+
+    override fun toError(e: Exception) = when (e) {
+        is HttpException -> Result.Error(
+            e.code(),
+            "$HTTP_ERROR_STR: ${e.response()?.errorBody()?.string() ?: e.message()}"
+        )
+
+        is IOException -> Result.Error(NETWORK_ERROR, getErrorMessage(NETWORK_ERROR, e.message))
+        else -> Result.Error(SYSTEM_ERROR, getErrorMessage(SYSTEM_ERROR, e.message))
     }
 }
