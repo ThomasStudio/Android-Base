8f2144efed96c5d1f6c643f0f26f3997791dfbe4
thomasmei <thomasmei@163.com>
2026-04-11 22:20:00 +0800

    add zhihu api call

--- app/src/main/java/com/thomas/androidbase/data/repositories/WeiboRepository.kt
+++ app/src/main/java/com/thomas/androidbase/data/repositories/WeiboRepository.kt
@@ -1,7 +1,10 @@
 package com.thomas.androidbase.data.repositories
 
 import com.thomas.androidbase.data.WeiboHot
+import com.thomas.androidbase.network.WeiboService
+import com.thomas.base.domain.Repository
 import com.thomas.base.domain.Result
+import javax.inject.Inject
 
 /**
  * Created by thomas on 4/11/2026.
@@ -9,4 +12,12 @@ import com.thomas.base.domain.Result
 
 interface WeiboRepository {
     suspend fun getWeiboHot(): Result<WeiboHot>
+}
+
+class WeiboRepositoryImpl @Inject constructor(
+    private val weiboService: WeiboService
+) : WeiboRepository, Repository() {
+    override suspend fun getWeiboHot(): Result<WeiboHot> {
+        return runCall { weiboService.getNews() }
+    }
 }
--- app/src/main/java/com/thomas/androidbase/data/repositories/ZhihuRepository.kt
+++ app/src/main/java/com/thomas/androidbase/data/repositories/ZhihuRepository.kt
@@ -1,7 +1,10 @@
 package com.thomas.androidbase.data.repositories
 
 import com.thomas.androidbase.data.ZhihuHot
+import com.thomas.androidbase.network.ZhihuService
+import com.thomas.base.domain.Repository
 import com.thomas.base.domain.Result
+import javax.inject.Inject
 
 /**
  * Created by thomas on 4/11/2026.
@@ -9,4 +12,12 @@ import com.thomas.base.domain.Result
 
 interface ZhihuRepository {
     suspend fun getZhihuHot(): Result<ZhihuHot>
+}
+
+class ZhihuRepositoryImpl @Inject constructor(
+    private val zhihuService: ZhihuService
+) : ZhihuRepository, Repository() {
+    override suspend fun getZhihuHot(): Result<ZhihuHot> {
+        return runCall { zhihuService.getHot() }
+    }
 }
