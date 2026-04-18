2a9f09b47e88319672bd3177d39b7e0bc5f66b49
thomasmei <thomasmei@163.com>
2026-04-18 17:20:33 +0800

    add components module

--- components/src/main/java/com/thomas/components/ui/ComboBoxExample.kt
+++ components/src/main/java/com/thomas/components/ui/ComboBoxExample.kt
@@ -7,8 +7,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -56,8 +54,7 @@ fun ComboBoxExample() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
-            .verticalScroll(rememberScrollState()),
+            .padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         Text(
