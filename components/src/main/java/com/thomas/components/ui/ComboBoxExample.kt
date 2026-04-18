355f909224ace662f6b397510f2ece97db350f7f
thomasmei <thomasmei@163.com>
2026-04-18 12:43:23 +0800

    add components module

--- components/src/main/java/com/thomas/components/ui/ComboBoxExample.kt
+++ components/src/main/java/com/thomas/components/ui/ComboBoxExample.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 /**
@@ -213,4 +214,10 @@ fun CustomObjectComboBoxExample() {
             style = androidx.compose.material3.MaterialTheme.typography.bodyMedium
         )
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ComboBoxExamplePreview() {
+    ComboBoxExample()
 }
