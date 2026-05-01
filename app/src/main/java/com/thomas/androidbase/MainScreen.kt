efede3f119b9adcbee2128ef709aa0211a9481b4
thomasmei <thomasmei@163.com>
2026-05-01 13:26:02 +0800

    add BaseDataViewModel

--- app/src/main/java/com/thomas/androidbase/MainScreen.kt
+++ app/src/main/java/com/thomas/androidbase/MainScreen.kt
@@ -1,6 +1,20 @@
 package com.thomas.androidbase
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -12,6 +26,8 @@ import com.thomas.androidbase.features.home.HomeScreen
 import com.thomas.androidbase.features.news.NewsScreen
 import com.thomas.androidbase.features.weibo.WeiboScreen
 import com.thomas.androidbase.navigation.MainRoute
+import com.thomas.base.navigation.DefaultNavigator
+import com.thomas.base.navigation.Navigator
 import com.thomas.base.navigation.asNavigator
 
 /**
@@ -21,6 +37,42 @@ import com.thomas.base.navigation.asNavigator
 @Composable
 fun MainScreen(navController: NavHostController) {
     val navigator = navController.asNavigator()
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            NavigationBar(navigator)
+        }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            MainContent(navController = navController, navigator)
+        }
+    }
+}
+
+@Composable
+fun NavigationBar(navigator: Navigator) {
+    NavigationBar {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            IconButton({ navigator.back() }) {
+                Icon(
+                    Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = null
+                )
+            }
+
+            Spacer(modifier = Modifier.weight(1f))
+        }
+    }
+
+}
+
+@Composable
+fun MainContent(navController: NavHostController, navigator: Navigator) {
     NavHost(navController = navController, startDestination = MainRoute.Home.path) {
         composable(MainRoute.Home.path) {
             HomeScreen(
@@ -51,4 +103,11 @@ fun MainScreen(navController: NavHostController) {
             )
         }
     }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun NavigationBarPreview() {
+    NavigationBar(DefaultNavigator())
 }
